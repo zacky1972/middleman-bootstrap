cc24df3fa6128eb41bb217fe056ab9f83659a1b2
zacky1972 <zacky1972@gmail.com>
2017-11-29 23:42:23 +0900

    install & set up bootstrap

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,6 @@
 // This is where it all goes :)
 
 require('jquery');
+require('popper.js');
+require('bootstrap');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,53 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
+const bootstrap = {
+	entry: [
+		__dirname + '/node_modules/bootstrap/scss/bootstrap.scss'
+	],
+	output: {
+		filename: 'bundle.css',
+		path: __dirname + '/build/stylesheets'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: function() {
+									return [
+										require("precss"),
+										require('autoprefixer')
+									];
+								}
+							}
+						},
+						'sass-loader'
+					]
+				})
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('css-loader')
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'file-loader?name=../font/[name].[ext]'
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('../stylesheets/bundle.css')
+	]
+}
+
+const sitejs = {
 	entry: [
 		'./source/javascripts/site.js'
 	],
@@ -8,11 +55,26 @@ module.exports = {
 		filename: 'bundle.js',
 		path: __dirname + '/build/javascripts'
 	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loaders: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|svg)$/,
+				loader: 'file-loader?name=../font/[name].[ext]'
+			}
+		]
+	},
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
     })
   ]
 };
 
+module.exports = [sitejs, bootstrap];
